4e9f7570082ceb882da681bb7f0f7c580c2cb75c
josuerodriguez1 <73971413+josuerodriguez1@users.noreply.github.com>
2020-12-02 08:09:22 -0500

    Update pythonTest.py

--- pythonTest.py
+++ pythonTest.py
@@ -1,2 +1,2 @@
-# This is just a very simple Hello World so I can actually have something in my repository :)
+# As my very first file on GitHub, this is just a very simple Hello World so I can actually have something in my repository :)
 print("Did you know that there are coffee houses in Russia where food and drink are free, but you pay for time?")
